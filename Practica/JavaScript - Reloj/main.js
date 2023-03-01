264db45f5ce5aa860590d74d2bd550121ddc4ef9
Gaston Rocha <gstnedgrdrch93@gmail.com>
2023-03-01 02:20:29 -0300

    LOGIN On

--- Practica/JavaScript - Reloj/main.js
+++ Practica/JavaScript - Reloj/main.js
@@ -1,2 +1,2 @@
 // Creamos un timer con "setInterval()" para hacer que una funcion se repita con un tiempo de retraso entre cada ejecucion. Se pasa dos parametros, la funcion que quieres llamar y el tiempo en milisegundos a retrasar cada ejecucionde la funcion. Se ejecutara hasta que sea borrada.
-setInterval(() => {},1000 );
+setInterval(() => {}, 1000);
